9c80f7477e86518a782e53dc7b0c8ca8a29be94e
RolandG369 <tup55624@temple.edu>
2025-02-19 20:19:51 -0500

    Updated Test Controller and Implemented a
    Got rid of the greetings so only /test is getting hit by Fetch API. Created an html file for the frontend that uses Bootstrap's CSS and JS bundles loaded from a CDN which gives us easy access to styling tools and interactive components like dropdowns. Implemented a JS alert to simulate a form submission and added a test button to call the /test endpoint.

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -6,11 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-	@GetMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!";
-	}
-
 	@GetMapping("/test")
 	public String test() {
 		return "If you see this, both the server and routing are working!";
